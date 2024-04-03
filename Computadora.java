4aa6b5bfb2667e15a63f95d5532cbe2b1da40229
Kruzxek <144373168+Kruzxek@users.noreply.github.com>
2024-04-03 16:07:12 -0600

    Update Computadora.java

--- Computadora.java
+++ Computadora.java
@@ -2,5 +2,14 @@ public class Computadora extends DispElect{
 
   private int ram;
   private String sistema;
-  
+
+  public Computadora (int id, int precio, String nombre, String marca, String pantalla, int ram, String sistema){
+    super(id, precio, nombre, marca, pantalla);
+    this.ram=ram;
+    this.sistema=sistema;
+  }
+
+  public String toString(){
+    return "Computadora";
+  }
 }
