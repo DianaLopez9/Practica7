e771738bc9afb42d0957e869b7e13f515741bcaf
Kruzxek <144373168+Kruzxek@users.noreply.github.com>
2024-04-03 12:41:14 -0600

    Update Movil.java

--- Movil.java
+++ Movil.java
@@ -1,3 +1,16 @@
 public class Movil extends DispElect{
+    
+  private String sistema;
+  private int ram;
+
+  public Movil (int id, int precio, String nombre, String marca, String pantalla, String sistema, int ram){
+    super(int id, int precio, String nombre, String marca, String pantalla);
+    this.sistema=sistema;
+    this.ram=ram;
+  }
+
+  public String toString(){
+    return "Movil";
+  }
 
 }
