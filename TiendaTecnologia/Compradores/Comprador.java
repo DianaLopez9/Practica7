d44a5781181073c5d7281fb67de089f48d3497dd
DianaLopez9 <162188126+DianaLopez9@users.noreply.github.com>
2024-04-05 00:27:13 -0600

    Update Comprador.java

--- TiendaTecnologia/Compradores/Comprador.java
+++ TiendaTecnologia/Compradores/Comprador.java
@@ -1,5 +1,28 @@
 package TiendaTecnologia.Compradores;
 
+import java.util.Random; 
+
 public class Comprador{
-	
+	private int id; 
+	private String nombre;
+	private CarritoDeCompras miCarrito;
+
+	Random rand = new Random();
+
+	public Comprador(String nombre){
+		this.id = rand.nextInt(10000);
+		this.nombre = nombre;
+		this.miCarrito = new CarritoDeCompras();
+	}
+
+    //????
+	/*public void mostrarMenu(){
+		System.out.println("Bienvenido al Menu");
+		System.out.println("Pc");
+		System.out.println("Laptop");
+	}*/
+
+	public void mostrarDescuentos(){
+		System.out.println("Estos son los descuentos disponibles");
+	}
 }
