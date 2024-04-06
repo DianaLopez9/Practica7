f56febbdc2932a1f322a0969914382f8dbe81966
DianaLopez9 <162188126+DianaLopez9@users.noreply.github.com>
2024-04-05 20:27:37 -0600

    Update Socio.java

--- TiendaTecnologia/Compradores/Socio.java
+++ TiendaTecnologia/Compradores/Socio.java
@@ -1,5 +1,18 @@
 package TiendaTecnologia.Compradores;
 
 public class Socio extends Comprador{
+	private String nombre;
+	private String apellido;
+	//private int 
+
+	public Socio(String nombre, String apellido){
+		super(nombre, apellido);
+	}
+
 	
+    public void mostrarPromociones() {
+    	System.out.println("¡Hola " + getNombre() + " Te mostramos las pomociones disponibles para socio:");
+    }
+
+
 }
