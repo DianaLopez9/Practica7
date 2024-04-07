0c5e235adaf12003fbc45c7420409ec83cb5602e
DianaLopez9 <162188126+DianaLopez9@users.noreply.github.com>
2024-04-07 12:28:53 -0600

    Update ClienteVip.java

--- TiendaTecnologia/Compradores/ClienteVip.java
+++ TiendaTecnologia/Compradores/ClienteVip.java
@@ -1,5 +1,19 @@
 package TiendaTecnologia.Compradores;
 
 public class ClienteVip extends Comprador{
-	
+    private int idVip;
+
+    public ClienteVip(String nombre, String apellido) {
+        super(nombre, apellido);
+    }
+
+    public int getIdVip() {
+        return idVip;
+    }
+
+    public void setIdVip(int idVip) {
+        this.idVip = idVip;
+    }
+    
+    
 }
