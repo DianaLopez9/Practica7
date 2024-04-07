eb8bb06d9230aa240339153d6b666a37894fecce
DianaLopez9 <162188126+DianaLopez9@users.noreply.github.com>
2024-04-07 17:15:09 -0600

    Update Smarthphone.java

--- TiendaTecnologia/dispositivos/Smarthphone.java
+++ TiendaTecnologia/dispositivos/Smarthphone.java
@@ -4,7 +4,7 @@ public class Smarthphone extends Movil{
     private int bateria;
     private String tamanio;
     
-    public Smartphone(int id, int precio, String nombre, String marca, String pantalla, String sistema, int ram, int bateria, String tamanio){
+    public Smartphone(int id, double precio, String nombre, String marca, String pantalla, String sistema, int ram, int bateria, String tamanio){
         super(id, precio, nombre, marca, pantalla,sistema,ram);
         this.bateria=bateria;
         this.tamanio=tamanio;
