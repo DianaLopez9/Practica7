0c043493bd5c13cf51b425897acf8f7e12bc81a8
Kruzxek <144373168+Kruzxek@users.noreply.github.com>
2024-04-06 19:11:49 -0600

    Update Estudiante.java

--- TiendaTecnologia/Compradores/Estudiante.java
+++ TiendaTecnologia/Compradores/Estudiante.java
@@ -1,5 +1,29 @@
 package TiendaTecnologia.Compradores;
 
 public class Estudiante extends Comprador{
+
+	private ArrayList<DispElect> carrito;
+	private String nombre;
+	private String apellido;
+	//private int 
+
+	public Socio(String nombre, String apellido){
+		super(nombre, apellido);
+		this.carrito = new ArrayList<>();
+	}
+
+	public void mostrarCarrito(){
+		for(){
+			
+		}
+
+	}
+
+	
+
+	
+	public void mostrarPromociones() {
+	    System.out.println("¡Hola " + getNombre() + " Te mostramos las pomociones disponibles para socio:");
+	}
 	
 }
