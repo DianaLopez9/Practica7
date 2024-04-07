cfea90d9a394efee1c03d4fb9e01843c19b06466
DianaLopez9 <162188126+DianaLopez9@users.noreply.github.com>
2024-04-07 17:14:50 -0600

    Update Tablet.java

--- TiendaTecnologia/dispositivos/Tablet.java
+++ TiendaTecnologia/dispositivos/Tablet.java
@@ -3,7 +3,7 @@ public class Tablet extends Smartphone {
 
     private String stylus;
     
-    public Tablet(int id, int precio, String nombre, String marca, String pantalla, String sistema, int ram, int bateria, String tamanio, String stylus){
+    public Tablet(int id, double precio, String nombre, String marca, String pantalla, String sistema, int ram, int bateria, String tamanio, String stylus){
         super(id, precio, nombre, marca, pantalla,sistema,ram,bateria,tamanio);
         this.stylus=stylus;
     }
