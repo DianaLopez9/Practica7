479538c9e35b155a236be8a0e13b35eb5d82dd32
Kruzxek <144373168+Kruzxek@users.noreply.github.com>
2024-04-08 03:30:33 -0600

    Update Smarthphone.java

--- TiendaTecnologia/dispositivos/Smarthphone.java
+++ TiendaTecnologia/dispositivos/Smarthphone.java
@@ -4,7 +4,7 @@ public class Smarthphone extends Movil{
     private int bateria;
     private String tamanio;
     
-    public Smartphone(double precio, String nombre, String marca, String pantalla, String sistema, int ram, int bateria, String tamanio){
+    public Smarthphone(double precio, String nombre, String marca, String pantalla, String sistema, int ram, int bateria, String tamanio){
         super(precio, nombre, marca, pantalla,sistema,ram);
         this.bateria=bateria;
         this.tamanio=tamanio;
@@ -14,9 +14,9 @@ public class Smarthphone extends Movil{
         return "Smartphone";
     }
     
-    public void mostraDatos(){
+    public void mostrarDatos(){
         super.mostrarDatos();
         System.out.println("Duracion de la bateria: "+this.bateria+" horas");
-        System.out.println(this.tamanio);
+        System.out.println("Tamanio: "+this.tamanio);
     }
 }
